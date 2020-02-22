86424a61501963118c523a0f879185415bd81b65
James Pearce <jamesquintanapearce@gmail.com>
2020-02-22 14:20:54 +1100

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,22 @@
 var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
 
-var connection = mysql.createConnection({
-    host: "localhost",
+        // Your port; if not 3306
+        port: 3306,
 
-    // Your port; if not 3306
-    port: 3306,
+        // Your username
+        user: "root",
 
-    // Your username
-    user: "root",
-
-    // Your password
-    password: "AggleRock",
-    database: "burgers_db"
-});
-
+        // Your password
+        password: "AggleRock",
+        database: "burgers_db"
+    });
+}
 connection.connect(function (err) {
     if (err) throw err;
 });
